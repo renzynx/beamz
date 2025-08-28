3a79bb81299405a4368c10bd1fa0068c74f1d555
renzynx <69468436+renzynx@users.noreply.github.com>
2025-08-28 22:21:53 +0700

    fix: update Toaster component styles for background and text colors

--- apps/web/src/components/ui/sonner.tsx
+++ apps/web/src/components/ui/sonner.tsx
@@ -12,8 +12,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       style={
         {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
+          "--normal-bg": "var(--card)",
+          "--normal-text": "var(--card-foreground)",
           "--normal-border": "var(--border)",
         } as React.CSSProperties
       }
